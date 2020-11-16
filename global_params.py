77529fd6c99f4b787fed65a1b026d1fae78331fb
Nikhil Kumar <nikhilkmr300@gmail.com>
2020-11-17 01:26:55 +0530

    Training starts, but no improvement in loss

--- global_params.py
+++ global_params.py
@@ -3,6 +3,6 @@ SEED = 1
 
 # ---------- TRAIN-TEST SPLIT ----------
 # Minimum number of images per class to consider for train and test.
-min_img_per_class = 10
+min_img_per_class = 3
 # If class contains min_img_per_class, fraction of total images in class to keep for testing.
 test_frac = 0.2
